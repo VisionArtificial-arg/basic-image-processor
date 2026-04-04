310214d64243393a86c151882a848153bc3beee0
Carlos Acuna <caacuna@mail.austral.edu.ar>
2026-04-04 15:54:30 -0300

    Add thresholds files to use into __init__.py

--- src/basic_image_processor/components/threshold/__init__.py
+++ src/basic_image_processor/components/threshold/__init__.py
@@ -1,1 +1,11 @@
-__all__ = []
+from .adaptive_gauss_threshold import AdaptiveGaussThreshold
+from .auto_threshold import AutomaticThreshold
+from .manual_threshold import ManualThreshold
+from .base import ThresholdApplier
+
+__all__ = [
+    "AdaptiveGaussThreshold",
+    "AutomaticThreshold",
+    "ManualThreshold",
+    "ThresholdApplier",
+]
